6349d4a051bbca960ae8078dc88f69835ddf265c
EMERY RODZIEWICZ <rod20012@byui.edu>
2023-11-09 07:46:38 -0500

    added PUT and DELETE

--- controller/reviews.js
+++ controller/reviews.js
@@ -18,7 +18,45 @@ const getSingle = async (req, res) => {
     });
 };
 
+//for put and delete endpoints
+const updateReview = async (req, res) => {
+    //#swagger.tags = ['reviews']
+    const reviewId = new ObjectId(req.params.id);
+    const review = {
+        listing_url: req.body.listing_url,
+        name: req.body.name,
+        summary: req.body.summary,
+        space: req.body.space,
+        access: req.body.access,
+        house_rules: req.body.house_rules,
+        property_type: req.body.property_type,
+        room_type: req.body.room_type,
+        price: req.body.price,
+        images: req.body.images
+    };
+    const response = await mongodb.getDatabase().db().collection('reviews').replaceOne( { _id: reviewId }, review);
+    if (response.modifiedCount > 0 ) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while updating user');
+    }
+};
+
+const deleteReview = async (req, res) => {
+    //#swagger.tags = ['reviews']
+    const reviewId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('reviews').deleteOne( { _id: reviewId });
+    if (response.deletedCount > 0 ) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred hwile updating the user.');
+    }
+};
+
+
 module.exports = {
     getAll,
-    getSingle
-}
+    getSingle,
+    updateReview,
+    deleteReview
+};
--- routes/reviews.js
+++ routes/reviews.js
@@ -6,5 +6,9 @@ const reviewsControllers = require('../controller/reviews');
 router.get('/', reviewsControllers.getAll);
 
 router.get('/:id', reviewsControllers.getSingle);
+//add put and delete endpoints
+router.put('/:id', reviewsControllers.updateReview);
+router.delete('/:id', reviewsControllers.deleteReview);
+
 
 module.exports = router;
