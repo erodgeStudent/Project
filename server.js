ad4e6e0894bc7ffd7ac1ac41d3212c5b85d653b9
EMERY RODZIEWICZ <rod20012@byui.edu>
2023-11-10 08:45:04 -0500

    put port back on. render.com link is not working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
-const port = process.env.port;
+const port = process.env.port || 3000;
 
 app.use(bodyParser.json());
 app.use('/', require('./routes'));
